fbc2a84b3e1e7f372b4710cdd6442de8a0eee579
felipet27 <felipet27@gmail.com>
2026-04-13 17:19:16 -0500

    Ver hora actual en DEVOPs

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 console.log("Hello, World! This is a simple JavaScript file.");
 
 console.log("You can add more functionality to this file as needed."); 
+
+// Crear una funcion que me pueda ver la hora actual
+function mostrarHoraActual() {
+    const fechaActual = new Date();
+    const hora = fechaActual.getHours();
+    const minutos = fechaActual.getMinutes();
+    const segundos = fechaActual.getSeconds();  
+    console.log(`La hora actual es: ${hora}:${minutos}:${segundos}`);
+}
+
+mostrarHoraActual();        
